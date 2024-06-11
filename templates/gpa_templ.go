75ccae6a05c7ec28f36f4d0168cbbfd2a8d0b12e
Dinesh Kumar G <gdineshda@gmail.com>
2024-06-11 14:21:33 +0530

    Fix minor bugs (resubmit, validation)

--- templates/gpa_templ.go
+++ templates/gpa_templ.go
@@ -26,7 +26,7 @@ func GPA(score types.GPA) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"cgpa_result_wrapper neon_text\"><div>Your CGPA for the Semester is ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"cgpa_result_wrapper neon_text cgpa_result\"><div>Your CGPA for the Semester is ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- templates/home_templ.go
+++ templates/home_templ.go
@@ -39,7 +39,7 @@ func Home() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"button_group_wrapper\"><button hx-target=\"previous .semester_list_wrapper\" hx-swap=\"beforeend\" hx-get=\"/semester/add\">Add Semester</button> <button hx-target=\"previous .semester_wrapper\" hx-swap=\"delete\" hx-delete=\"/semester/delete\">Delete Semester</button> <button type=\"submit\" name=\"cpga_form\" hx-post=\"/cgpa\" hx-target=\"#cgpa_result\" hx-swap=\"outerHTML\">Submit</button></div></form><div id=\"cgpa_result\"></div></div></body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"button_group_wrapper\"><button hx-target=\"previous .semester_list_wrapper\" hx-swap=\"beforeend\" hx-get=\"/semester/add\">Add Semester</button> <button hx-target=\"previous .semester_wrapper\" hx-swap=\"delete\" hx-delete=\"/semester/delete\">Delete Semester</button> <button type=\"submit\" name=\"cpga_form\" hx-post=\"/cgpa\" hx-target=\".cgpa_result\" hx-swap=\"outerHTML\">Submit</button></div></form><div class=\"cgpa_result\"></div></div></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -87,7 +87,7 @@ func Subject() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"subject_wrapper\"><div class=\"subject_code_wrapper form_item\"><label>Subject Code</label> <input type=\"text\" name=\"subject_code\"></div><div class=\"subject_credit_wrapper form_item\"><label>Subject Credit(s)</label> <input type=\"number\" name=\"subject_credit\"></div><div class=\"subject_grade_wrapper form_item\"><label>Subject Grade</label> <select name=\"subject_grade\"><option value=\"10\">O</option> <option value=\"9\">A+</option> <option value=\"8\">A</option> <option value=\"7\">B+</option> <option value=\"6\">B</option> <option value=\"5\">C</option></select></div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"subject_wrapper\"><div class=\"subject_code_wrapper form_item\"><label>Subject Code</label> <input type=\"text\" name=\"subject_code\"></div><div class=\"subject_credit_wrapper form_item\"><label>Subject Credit(s)</label> <input min=\"1\" max=\"100\" type=\"number\" name=\"subject_credit\"></div><div class=\"subject_grade_wrapper form_item\"><label>Subject Grade</label> <select name=\"subject_grade\"><option value=\"10\">O</option> <option value=\"9\">A+</option> <option value=\"8\">A</option> <option value=\"7\">B+</option> <option value=\"6\">B</option> <option value=\"5\">C</option></select></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
